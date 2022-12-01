388214fc8015844dab9b3c1e2c5506a2f099a5b7
steve_loew <stevecloew@gmail.com>
2022-12-01 10:18:34 -0700

    day_1B

--- advent.py
+++ advent.py
@@ -2,18 +2,23 @@
 Solutions to 2022 advent of code
 https://adventofcode.com/
 """
+DELIMITER = '\n===============\n'
 
 
-def day_1():
+def day_1(part='A'):
+    """
+    :param part: for polymorphism; 'A' for part A 'B' for B
+    :return:
+    """
     with open(r'inputs/DAY_1A.txt') as infile:
         data = infile.read().split('\n\n')
 
-    for entry in data[:-1]:
-        max_ = max(max_, sum(int(i) for i in entry.split('\n')))
-    max_ = max(sum(int(i) for i in entry.split('\n')) for entry in data[:-1])
-    print(f'DAY_1A={max_}')
-    return max_
+    cals = [sum(int(i) for i in entry.split('\n')) for entry in data[:-1]]
+    cals.sort()
+    return sum(cals[-1 if part.lower() == "a" else -3:])
 
 
 if __name__ == '__main__':
-    day_1()
+    print(f'Day_1A={day_1()}')
+    print(f'Day_2A={day_1(part="B")}')
+    print(DELIMITER)
